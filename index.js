03071567aeeb62233ab7f1821ac4524c028583d7
unknown <mohamedazaam202@gmail.com>
2026-09-09 15:43:36 +0300

    up

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ async function fetchWeatherData(city) {
   getWindElement.innerHTML = "";
   try {
     const fetchData = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=c5ac0e6d85b24f6394601824262208&q=${city}&days=7`,
+      `https://api.weatherapi.com/v1/forecast.json?key=818e3805c6844c60bc9120115260909&q=${city}&days=7`,
     );
     const data = await fetchData.json();
     document.images[0].src = "https:" + data.current.condition.icon;
